7ef60f07dfaab803ef10b8265ef5bbac2412a49e
Caprico85 <Caprico85@users.noreply.github.com>
2022-10-15 22:44:53 +0200

    Dateien für Traits

---
+++ devices/traits/startstop.js
@@ -0,0 +1,31 @@
+/**
+ * NodeRED Google SmartHome
+ * Copyright (C) 2022 Andreas Schuster.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+"use strict";
+
+const Base = require('./base.js');
+
+/**
+ * StartStop trait
+ *
+ * @see https://developers.google.com/assistant/smarthome/traits/startstop
+ */
+class StartStop extends Base {
+}
+
+module.exports = StartStop;
